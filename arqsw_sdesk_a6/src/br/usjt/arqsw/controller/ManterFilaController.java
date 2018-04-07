0798c94106dbeb7188f6f1b609e81ab8c196669c
Jose <jose.roberto.xavier.s@gmail.com>
2018-04-07 09:39:20 -0300

    CRUD Fila

---
+++ arqsw_sdesk_a6/src/br/usjt/arqsw/controller/ManterFilaController.java
@@ -0,0 +1,43 @@
+package br.usjt.arqsw.controller;
+
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.usjt.arqsw.entity.Fila;
+import br.usjt.arqsw.service.FilaService;
+
+@Transactional
+@Controller
+public class ManterFilaController {
+	
+	private FilaService fs;
+	
+	@Autowired
+	public ManterFilaController(FilaService fs){
+		this.fs = fs;
+	}
+	
+	@RequestMapping("index")
+	public String inicio() {
+		return "index";
+	}
+	
+	@RequestMapping("criar_fila")
+	public String criarChamado(@Valid Fila f, BindingResult result, Model model ) throws IOException{
+
+		f.setNome("nome");
+		fs.criar(f);
+		return "index";
+		
+	}
+	
+
+}
